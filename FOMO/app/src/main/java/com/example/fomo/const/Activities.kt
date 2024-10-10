99ebfd23fa0abe5d7c8fc5ac48bf9bf6954885de
Alan Chung <alanchung2k2@gmail.com>
2024-10-10 02:37:03 -0400

    activities added

---
+++ FOMO/app/src/main/java/com/example/fomo/const/Activities.kt
@@ -0,0 +1,41 @@
+package com.example.fomo.const
+
+import com.example.fomo.models.Activity
+
+// might move this to db?
+val activities =
+    listOf(
+        Activity("Working Out", "\uD83D\uDCAA"),
+        Activity("Studying", "\uD83D\uDCDA"),
+        Activity("Home", "\uD83C\uDFE0"),
+        Activity("Social", "\uD83D\uDDE3\uFE0F"),
+        Activity("Idle", "\uD83D\uDCA4"),
+        Activity("Game Night", "\uD83C\uDFAE"),
+        Activity("Chilling", "\uD83D\uDE0C"),
+        Activity("Walking", "\uD83D\uDEB6"),
+        Activity("Dinner Party", "\uD83C\uDF7D\uFE0F"),
+        Activity("Sleeping", "\uD83D\uDE34"),
+        Activity("Do Not Disturb", "\uD83C\uDF19"),
+        Activity("On My Way", "\uD83D\uDD1C"),
+        Activity("Movie Night", "\uD83C\uDFA6"),
+        Activity("Reading", "\uD83D\uDCD6"),
+        Activity("Cooking/Baking", "\uD83C\uDF5E"),
+        Activity("Potluck", "\uD83C\uDF72"),
+        Activity("Dance", "\uD83C\uDF72"),
+        Activity("Picnic", "\uD83E\uDDFA"),
+        Activity("Hiking", "⛰\uFE0F"),
+        Activity("Sports", "⚽"),
+        Activity("Swimming", "\uD83C\uDFCA"),
+        Activity("Golfing", "⛳"),
+        Activity("BBQ", "\uD83C\uDF56"),
+        Activity("Camping", "⛺"),
+        Activity("Cycling", "\uD83D\uDEB2"),
+        Activity("Kayaking", "\uD83D\uDEA3\u200D♀\uFE0F"),
+        Activity("Fishing", "\uD83C\uDFA3"),
+        Activity("Gardening", "\uD83C\uDF3B"),
+        Activity("Concerts", "\uD83C\uDFA4"),
+        Activity("House Party", "\uD83C\uDF89"),
+        Activity("Bar Hopping", "\uD83C\uDF77"),
+        Activity("Lectures", "\uD83C\uDFEB"),
+        Activity("Working", "\uD83D\uDCBB"),
+    )
