5f64a7b3585fac35d24da692c4669111b2b8ee26
s2bai <s2bai@uwaterloo.ca>
2024-10-30 22:06:54 -0400

    database refactor, map re-centering on switch

--- FOMO/app/src/main/java/com/example/fomo/models/MyViewModel.kt
+++ FOMO/app/src/main/java/com/example/fomo/models/MyViewModel.kt
@@ -1,11 +1,48 @@
 package com.example.fomo.models
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.lifecycle.viewModelScope
+import com.example.fomo.BuildConfig
+import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.postgrest.Postgrest
+import io.github.jan.supabase.postgrest.from
+import kotlinx.coroutines.launch
 
 class MyViewModel : ViewModel() {
+
+    val supabase = createSupabaseClient(
+        supabaseUrl = "https://vwapghztewutqqmzaoib.supabase.co",
+        supabaseKey = BuildConfig.SUPABASE_KEY
+    ) {
+        install(Postgrest)
+    }
+    // State to hold the list of users
+    var users by mutableStateOf<List<User>>(emptyList())
+        private set
+
+    // State to hold the list of friendships
+    var friendships by mutableStateOf<List<Friendship>>(emptyList())
+        private set
+
+    fun fetchDatabase() {
+        viewModelScope.launch {
+            try {
+                val userRes = supabase.from("users").select().decodeList<User>()
+                val friendshipRes = supabase.from("friendship").select().decodeList<Friendship>()
+                users = userRes
+                friendships = friendshipRes
+
+                Log.d("SupabaseConnection", "Friendships fetched: $friendships")
+                Log.d("SupabaseConnection", "Users fetched: $users")
+            } catch (e: Exception) {
+                Log.e("SupabaseConnection", "Failed to connect to database: ${e.message}")
+            }
+        }
+    }
 
     var displayName by mutableStateOf("Kevin Yang") // Initial display name
 
