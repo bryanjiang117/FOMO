7e0f39f37029f13ce55c3e6a6fa305394d660ae5
Bryan Jiang <haozhoujiang@gmail.com>
2024-10-31 23:24:45 -0400

    feat: Added status to map

--- FOMO/app/src/main/java/com/example/fomo/MapScreen.kt
+++ FOMO/app/src/main/java/com/example/fomo/MapScreen.kt
@@ -4,14 +4,23 @@ import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
+import com.example.fomo.const.Colors
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.rememberCameraPositionState
@@ -90,8 +99,28 @@ fun Map(mapViewModel: MapViewModel, myViewModel: MyViewModel) {
       }
     }
 
+    Card(
+      modifier = Modifier
+        .align(Alignment.TopEnd) // Position card at the top center
+        .padding(16.dp), // Padding from the top or screen edges
+      colors = CardDefaults.cardColors(
+        containerColor = Color.White
+      ),
+      shape = RoundedCornerShape(24.dp),
+    ) {
+      Column(
+        modifier = Modifier.padding(horizontal = 24.dp, vertical = 16.dp)
+      ) {
+        Text(
+          text = "${myViewModel.activity.name} ${myViewModel.activity.emoji}",
+          color = Color.Black,
+        )
+//        Text(
+//          text = "MC",
+//          color = Color.Black,
+//        )
+      }
+    }
 
-
-    // ...
   }
 }
