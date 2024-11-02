905cbaf9957bec58c684df9e42649a1d445ba304
Alan Chung <alanchung2k2@gmail.com>
2024-11-01 20:29:43 -0400

    minor changes

--- FOMO/app/src/main/java/com/example/fomo/models/Status.kt
+++ FOMO/app/src/main/java/com/example/fomo/models/Status.kt
@@ -8,5 +8,5 @@ data class Status(
   @SerialName("id") val id: Long,                             // Primary key for the status
   @SerialName("created_at") val createdAt: String,    // Timestamp for when the status was created
   @SerialName("description") val description: String,   // Status' description
-  @SerialName("emoji") val emoji: String? = "",          // Status' emoji
+  @SerialName("emoji") val emoji: String,          // Status' emoji
 )
