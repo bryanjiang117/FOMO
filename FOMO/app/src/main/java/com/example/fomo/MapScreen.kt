0dea6e4ffa97ae3deb108c15fef1f932986bc880
Bryan Jiang <haozhoujiang@gmail.com>
2024-11-13 17:27:40 -0500

    fix: fixed place flag icon inside transparent

--- FOMO/app/src/main/java/com/example/fomo/MapScreen.kt
+++ FOMO/app/src/main/java/com/example/fomo/MapScreen.kt
@@ -12,8 +12,10 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.DirectionsWalk
+import androidx.compose.material.icons.filled.Circle
 import androidx.compose.material.icons.filled.DirectionsBus
 import androidx.compose.material.icons.filled.DirectionsCar
+import androidx.compose.material.icons.filled.Flag
 import androidx.compose.material.icons.filled.FlagCircle
 import androidx.compose.material.icons.filled.PedalBike
 import androidx.compose.material3.Button
@@ -30,6 +32,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -73,8 +76,16 @@ fun CustomMapMarker(
     title = name,
     anchor = Offset(0.5f, 1f),
   ) {
-    Icon(imageVector = Icons.Default.FlagCircle, contentDescription = name, tint = color)
-    // this flag icon ^^ is so troll. the center is transparent bruh
+    Icon(
+      imageVector = Icons.Default.Circle,
+      contentDescription = name, tint = color,
+      modifier = Modifier.scale(1.2f)
+    )
+    Icon(
+      imageVector = Icons.Default.Flag,
+      contentDescription = name, tint = Color.White,
+      modifier = Modifier.scale(0.7f)
+    )
   }
 }
 
