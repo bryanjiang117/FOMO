0a75692a3d537b508ba806b21436d764bf7a2abf
Alan Chung <alanchung2k2@gmail.com>
2024-12-02 22:33:43 -0500

    change password, app icon

--- FOMO/app/src/main/java/com/example/fomo/entities/Game.kt
+++ FOMO/app/src/main/java/com/example/fomo/entities/Game.kt
@@ -6,9 +6,9 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class Game(
   @SerialName("id") val id: Long? = null,                                 // Primary key
-  @SerialName("start_time") val startTime: String? = null, // Timestamp for creation date
-  @SerialName("end_time") val endTime: String? = null, // Timestamp for creation date
+  @SerialName("start_time") val startTime: String? = null,
+  @SerialName("end_time") val endTime: String? = null,
   @SerialName("group_id") val groupId: Long,
-  @SerialName("hunter_id") val hunterId: String? = null,  // ID of the user who created the group
-  @SerialName("running") val running: Boolean? = null,  // ID of the user who created the group
+  @SerialName("hunter_id") val hunterId: String? = null,
+  @SerialName("running") val running: Boolean? = null,
 )
